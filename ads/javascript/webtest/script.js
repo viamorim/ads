ab69165c6dacf9c4be19a3303a7fbfb371dea350
viamorim <victoramorimbrandao.learning@gmail.com>
2024-05-02 03:25:20 -0300

    Calculo automatico da idade

--- ads/javascript/webtest/script.js
+++ ads/javascript/webtest/script.js
@@ -1,9 +1,12 @@
+//input
 anoNascimento = 2000
-
-idade = 2024 - anoNascimento
-
 nome = "Victor"
 
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
 mensagem = "Sou " + nome + " e tenho " + idade + " anos"
 
+//output
 alert(mensagem)
