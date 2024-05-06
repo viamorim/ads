b46599ee05350abe8d7e55820b2eca4b943c62e4
viamorim <victoramorimbrandao.learning@gmail.com>
2024-05-06 03:31:41 -0300

    Campo de valor adicional incluso

--- ads/javascript/webtest/script.js
+++ ads/javascript/webtest/script.js
@@ -1,12 +1,23 @@
 //input
-anoNascimento = 2000
-nome = "Victor"
+anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
+nome = prompt("Informe o seu nome")
+salarioBase = parseFloat(prompt("Informe o salário base"))
+gratificacao = parseFloat(prompt("Informe a gratificação"))
+bonus = parseFloat(prompt("Informe o bônus"))
+desconto = parseFloat(prompt("Informe o desconto"))
+
+salarioLiquido = 0
+
+//criar com 'if' variaval para pessoas com até 20 anos receberem +100 e pessoas com mais de 20 anos receberem +2000
+adicional = 0
 
 //processamento
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
 mensagem = "Sou " + nome + " e tenho " + idade + " anos"
+salarioLiquido = salarioBase + gratificacao + bonus - desconto
 
 //output
+mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido + adicional
 alert(mensagem)
