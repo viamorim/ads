7fc8e2c380a403a607602b8b031925c46023f5f9
viamorim <victoramorimbrandao.learning@gmail.com>
2024-05-06 06:38:56 -0300

    Criando constantes, let e if

--- ads/javascript/webtest/script.js
+++ ads/javascript/webtest/script.js
@@ -1,23 +1,31 @@
+//constantes
+const salario_ate_20 = 1000
+const salario_acima_20 = 2000
+const idade_limite = 20
+
 //input
-anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
-nome = prompt("Informe o seu nome")
-salarioBase = parseFloat(prompt("Informe o salário base"))
-gratificacao = parseFloat(prompt("Informe a gratificação"))
-bonus = parseFloat(prompt("Informe o bônus"))
-desconto = parseFloat(prompt("Informe o desconto"))
+const anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
+const nome = prompt("Informe o seu nome")
+const salarioBase = parseFloat(prompt("Informe o salário base"))
+const gratificacao = parseFloat(prompt("Informe a gratificação"))
+const bonus = parseFloat(prompt("Informe o bônus"))
+const desconto = parseFloat(prompt("Informe o desconto"))
 
-salarioLiquido = 0
-
-//criar com 'if' variaval para pessoas com até 20 anos receberem +100 e pessoas com mais de 20 anos receberem +2000
+//criar com 'if' variaval para pessoas com até 20 anos receberem +1000 e pessoas com mais de 20 anos receberem +2000
 adicional = 0
 
 //processamento
-hoje = new Date()
-anoAtual = hoje.getFullYear()
-idade = anoAtual - anoNascimento
-mensagem = "Sou " + nome + " e tenho " + idade + " anos"
-salarioLiquido = salarioBase + gratificacao + bonus - desconto
+const hoje = new Date()
+const anoAtual = hoje.getFullYear()
+const idade = anoAtual - anoNascimento
+
+let adicional = salario_acima_20
+if(idade <= idade_limite) {
+	adicional = salario_ate_20
+}
+
+let salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
 
 //output
-mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido + adicional
+let mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido
 alert(mensagem)
